bfe402b99e2426d5495c1f405b0db740e2719998
Бекжан <bekgan@macbook-pro-bekgan.local>
2019-11-15 17:44:15 +0600

    Initial Commit

---
+++ Test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by Bekgan on 15.11.2019.
+//  Copyright © 2019 Bekgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
